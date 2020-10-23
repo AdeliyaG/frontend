e62719396eaab024e9ec3201a8287fb1cb0c3c2c
Adeliya <adeliyanazipova@mail.ru>
2020-10-23 19:10:35 +0300

    final version

--- test-prj-technaxis/app/index.js
+++ test-prj-technaxis/app/index.js
@@ -1,10 +1,4 @@
-toggle.onclick = function f() {
-    let x = document.getElementById("sidebar");
-    console.log(x.className)
-
-    if (x.className === "sidebar") {
-        x.className += " active";
-    } else {
-        x.className = "sidebar";
-    }
+document.getElementById("toggle").onclick = function navToggle() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active")
 }
